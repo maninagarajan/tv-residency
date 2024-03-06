4ff7060fcf111a880939c33e631c40a779629a92
nagamani <v-nagamani@damacgroup.com>
2024-03-06 13:32:27 +0530

    about temple screen

--- src/screens/AboutTemple/aboutTemple.jsx
+++ src/screens/AboutTemple/aboutTemple.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import Navbar from '../../components/TopNavBar/navBar';
 import './aboutTemple.css';
+import TopImg from '../../assets/images/AboutTemple/Temple_Top.png';
+import GoddessImg from '../../assets/images/AboutTemple/Goddess.png';
+import TempleImg from '../../assets/images/AboutTemple/Temple.png';
+import { aboutGod, aboutTemple, newMoon } from '../../constants/info';
 
 export const AboutTempleComponent = () => {
     return (
         <div className='container'>
             <Navbar />
-            <h1>About Temple</h1>
+            <div className='screenTop'>
+                <img src={TopImg} alt='Contact-Us' />
+                <div>
+                    <h2>About temple</h2>
+                </div>
+            </div>
+
+            <div className='aboutTempleDiv'>
+                <h1 className='roomSectionHeaderText'>General Information</h1>
+
+                <div className='imageTextRow'>
+                    <img src={GoddessImg} alt='Goddess' />
+                    <p>{aboutGod}</p>
+                </div>
+
+                <h1 className='roomSectionHeaderText'>Temple</h1>
+                <div className='imageTextRow'>
+                    <div>
+                        {aboutTemple?.map((line) => <p key={line}>{`${line}\n\n`}</p>)}
+                    </div>
+                    <img src={TempleImg} alt='temple' />
+                </div>
+
+                <h1 className='roomSectionHeaderText'>The ‘New’ Moon</h1>
+                <div className='imageText'>
+                    {newMoon?.map((line) => <p key={line}>{`${line}\n\n`}</p>)}
+                </div>
+            </div>
         </div>
     )
 }
