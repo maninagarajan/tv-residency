69963a223221634314f9494db0e279781fdc73ed
nagamani <v-nagamani@damacgroup.com>
2024-03-06 19:27:05 +0530

    scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
+import ScrollToTop from './routes/scrollToTop.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter basename="/">
-      <App />
-    </HashRouter>
+  <HashRouter basename="/">
+    <ScrollToTop />
+    <App />
+  </HashRouter>
 )
