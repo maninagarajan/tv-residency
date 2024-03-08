c353b10a18c1fc41669991df1b3cc649f1adaff9
nagamani <v-nagamani@damacgroup.com>
2024-03-08 13:46:50 +0530

    footer click action

--- src/components/FooterComponent/footer.jsx
+++ src/components/FooterComponent/footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './footer.css';
 import LogoImg from '../../assets/images/Logo_White.png'
-import { address, email, phoneNumber, whatsAppNumber } from '../../constants/info';
+import { address, email, phoneNumber, whatsAppLink, whatsAppNumber } from '../../constants/info';
 import locationIcon from '../../assets/images/location.png'
 import phoneIcon from '../../assets/images/phone.png'
 import whatsappIcon from '../../assets/images/whatsapp.png'
 import mailIcon from '../../assets/images/mail.png'
+import { Tooltip } from '@mui/material';
 
 export const FooterComponent = () => {
     return (
@@ -29,17 +30,23 @@ export const FooterComponent = () => {
                     <h3>Contact</h3>
                     <div className='locationRow'>
                         <img src={phoneIcon} alt={'phone-icon'} />
-                        <p>{phoneNumber}</p>
+                        <Tooltip title="Open Dialer" placement='right'>
+                            <p className='whatsappNumber' onClick={() => window.open('tel:' + phoneNumber)}>{phoneNumber}</p>
+                        </Tooltip>
                     </div>
 
                     <div className='locationRow'>
                         <img src={whatsappIcon} alt={'whatsapp-icon'} />
-                        <p>{whatsAppNumber}</p>
+                        <Tooltip title="Open Whatsapp" placement='right'>
+                            <p className='whatsappNumber' onClick={() => window.open(whatsAppLink)}>{whatsAppNumber}</p>
+                        </Tooltip>
                     </div>
 
                     <div className='locationRow'>
                         <img src={mailIcon} alt={'email-icon'} />
-                        <p>{email}</p>
+                        <Tooltip title="Open Mail" placement='right'>
+                            <p className='whatsappNumber' onClick={() => window.open('mailto:' + email)}>{email}</p>
+                        </Tooltip>
                     </div>
                 </div>
             </div>
