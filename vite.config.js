f95bd3f5eba0aba53820e3e6b3db00611841fbc6
nagamani <v-nagamani@damacgroup.com>
2024-03-18 10:01:41 +0530

    vite pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -4,35 +4,33 @@ import react from '@vitejs/plugin-react'
 import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 const manifest = {
-  "short_name": "TV Residency",
-  "name": "Temple View Residency",
-  "icons": [
+  short_name: "TV Residency",
+  name: "Temple View Residency",
+  icons: [
     {
-      "src": "public/favicon.png",
-      "sizes": "64x64 32x32 24x24 16x16",
-      "type": "image/png"
+      src: "public/favicon.png",
+      sizes: "64x64 32x32 24x24 16x16",
+      type: "image/png"
     },
     {
-      "src": "public/icon192.png",
-      "type": "image/png",
-      "sizes": "192x192"
+      src: "public/icon192.png",
+      type: "image/png",
+      sizes: "192x192"
     },
     {
-      "src": "public/icon512.png",
-      "type": "image/png",
-      "sizes": "512x512"
+      src: "public/icon512.png",
+      type: "image/png",
+      sizes: "512x512"
     }
   ],
-  "start_url": ".",
-  "display": "standalone",
-  "theme_color": "#c2700f",
-  "background_color": "#fbf8ef",
-  "theme-color": "#c2700f",
-  "background-color": "#fbf8ef"
+  start_url: ".",
+  display: "standalone",
+  theme_color: "#c2700f",
+  background_color: "#fbf8ef"
 }
 
 export default defineConfig({
-  plugins: [viteCommonjs(), react(), VitePWA(manifest)],
+  plugins: [viteCommonjs(), react(), VitePWA({ manifest: manifest })],
   build: {
     outDir: 'build',
   },
